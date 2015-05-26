b9a20797e52b9954118adcee1320f1817843a2dc
Brian Huynh <brianhuynh1@gmail.com>
2015-05-26 10:11:27 -0700

    Changed name of home section view controller

---
+++ MobileOtter/MobileOtter/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  MobileOtter
+//
+//  Created by Brian Huynh on 5/26/15.
+//  Copyright (c) 2015 Brian Huynh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+
+}
