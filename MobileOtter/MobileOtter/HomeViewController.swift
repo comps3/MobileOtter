6a991cdfe773b1c9dc8475382f116c15ae751df2
Brian Huynh <brianhuynh1@gmail.com>
2015-05-26 11:11:49 -0700

    Changed Navigation Bar UI

--- MobileOtter/MobileOtter/HomeViewController.swift
+++ MobileOtter/MobileOtter/HomeViewController.swift
@@ -10,5 +10,15 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Red, Green and Blue values of Navigation Bar
+        // Initializer only takes values from 0.0 to 1.0
+        let redValue = CGFloat(34.0/255.0)
+        let greenValue = CGFloat(52.0/255.0)
+        let blueValue = CGFloat(106.0/255.0)
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
 
 }
